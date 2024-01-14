22491e8e6c57b763958acb3faaa2dac08395d2c8
sinhaugoh <sinhaugoh456@hotmail.com>
2024-01-14 17:29:28 +0800

    feat: implement signin endpoint

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
-@Injectable({})
+@Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  signin() {
-    return 'Sign in';
+  async signup(dto: AuthDto) {
+    // generate the password hash
+    const hash = await argon.hash(dto.password);
+
+    // save the new user in the db
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+
+      delete user.hash;
+
+      return user;
+    } catch (e) {
+      if (e instanceof PrismaClientKnownRequestError) {
+        if (e.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+
+      throw e;
+    }
   }
 
-  signup() {
-    return 'Sign up';
+  signin() {
+    return 'Sign in';
   }
 }
